a5946922e06c0bbefcfb33327683233b4eace20c
DoeMaarWel <vandismc@gmail.com>
2024-12-03 23:16:48 +0100

    Add files via upload

---
+++ pg2.js
@@ -0,0 +1,27 @@
+
+const Chosices = ["Steen", "Papier", "Schaar"];
+const PlayerDisplay = document.getElementById("PlayerDisplay");
+const ComputerDisplay = document.getElementById("ComputerDisplay");
+const ResultDisplay = document.getElementById("ResultDisplay");
+function PlayGame(PlayerChoices){
+const ComputerChoices = Chosices[Math.floor(Math.random() * 3)];
+let Result = "";
+if (PlayerChoices === ComputerChoices) {
+Result = "Gelijkspel!";} 
+else {
+switch (PlayerChoices) {
+case "Steen":
+Result = (ComputerChoices === "Schaar") ? "Je hebt GEWONNEN!!!!" : "Je hebt verloren";
+break;
+case "Papier":
+Result = (ComputerChoices === "Steen") ? "Je hebt GEWONNEN!!!!" : "Je hebt verloren";
+break;
+case "Schaar":
+Result = (ComputerChoices === "Papier") ? "Je hebt GEWONNEN!!!!" : "Je hebt verloren";
+break;
+}
+}
+PlayerDisplay.textContent = `Speler: ${PlayerChoices}`;
+ComputerDisplay.textContent = `Bot: ${ComputerChoices}`;
+ResultDisplay.textContent = Result;
+}
